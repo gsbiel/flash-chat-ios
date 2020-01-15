f4a385912bc1e52668ba4eb3f4dc63d9d6bd7d86
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-15 06:20:45 -0500

    Inserindo elementos na tableView da tela de chat

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,8 +14,16 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let messages : [Message] = [
+        Message(sender: "1@2.com", body: "Hello!"),
+        Message(sender: "a@b.com", body: "Heey!"),
+        Message(sender: "1@2.com", body: "How's it going?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //tableView.delegate = self // seria usado se quisessemos inserir interacao com as linhas da tabela.
+        tableView.dataSource = self
         title = Constants.appName
         navigationItem.hidesBackButton = true
 
@@ -35,3 +43,32 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+
+// Protocolo responsavel por preencher a tabela.
+extension ChatViewController: UITableViewDataSource {
+    
+    // Esse metodo e chamado pelo TableView. Este, espera que o metodo retorne um inteiro indicando quantas linhas devem ser alocadas para preencher a tabela.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    // Esse metodo e chamado pelo TableView. Este, espera que o metodo retorne um TableViewCell para inserir na tabela.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+//// Protocolo responsavel pelas interacoes com a tabela.
+//extension ChatViewController: UITableViewDelegate {
+//
+//    // Esse metodo e chamado sempre que o usuario seleciona uma das linhas da tabela!
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//
+//}
+
+
