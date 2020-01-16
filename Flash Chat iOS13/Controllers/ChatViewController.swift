c701d4b32ff84743c6d30c6232ef2345485ea32a
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-16 18:36:03 -0500

    Ordenando as informacoes recebidas do database

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -37,7 +37,9 @@ class ChatViewController: UIViewController {
     
     func loadMessages() {
         messages = []
-        db.collection(Constants.FStore.collectionName).getDocuments { (querySnapshot, error) in
+        db.collection(Constants.FStore.collectionName)
+            .order(by: Constants.FStore.dateField)
+            .getDocuments { (querySnapshot, error) in
             if let e = error {
                 print("There was an issue retrieving data from the FireStore. \(e)")
             }else {
@@ -65,7 +67,11 @@ class ChatViewController: UIViewController {
         
         //Quando o usuario aperta o bot'ao de enviar mensagem, a mesma deve ser enviada para o database
         if let messageBody = messageTextfield.text, let messageSender =  Auth.auth().currentUser?.email {
-            db.collection(Constants.FStore.collectionName).addDocument(data: [Constants.FStore.senderField : messageSender, Constants.FStore.bodyField : messageBody]) { (error) in
+            db.collection(Constants.FStore.collectionName).addDocument(data: [
+                Constants.FStore.senderField : messageSender,
+                Constants.FStore.bodyField : messageBody,
+                Constants.FStore.dateField : Date().timeIntervalSince1970
+            ]) { (error) in
                 if let e = error {
                     print("There was an issue saving data to firestore. \(e)")
                 }else {
